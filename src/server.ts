ef625d7e9aabbf2b5c39d58ae370d05ac1354581
mike030188 <halol.imnida2023@gmail.com>
2024-02-25 06:57:20 +0500

    feat: Integration of enviranmental variables

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,42 @@
+// burak lohiyamizda packagelarni "ES2020" orqali chaqiramiz:
 
+import dotenv from "dotenv"; // const dotenv = require ( 'dotenv'); (*commonjs sample FYI:)
+dotenv.config();
+
+// //** PORT ni tekwiriw */
+// console.log("PORT:", process.env.PORT);
+
+// //** DataBase ni tekwiriw */
+// console.log("MONGO_URL:", process.env.MONGO_URL);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// *** Types of pattern ***
+// Architectoral pattern: MVC, Dependency Injection (DI), MVP 
+
+// Design pattern: Middleware, Decorator
 
 
 // console.log("EXECUTED!");
