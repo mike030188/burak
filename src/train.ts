43fb1a2c526f1a180facb255aa12c8700078695f
mike030188 <halol.imnida2023@gmail.com>
2024-02-27 20:30:39 +0500

    task: MIT task-I is done

--- src/train.ts
+++ src/train.ts
@@ -1,15 +1,31 @@
 console.log("Hello World");
 
-// *** H - TASK ***
-// Shunday function tuzing, u string qabul qilib teskari qilib return qilsin
-// MASALAN: gerReverse("hello") return qiladi "olleh"
-function getReverse(text: string) {
+// *** I - TASK ***
+// shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib,
+//  faqat positive qiymatlarni olib string holatda return qilsin.
+// MASALAN: getPositive([1, -4, 2]) return qiladi "12"
 
-    const Text = text.split('').reverse().join('');
-    return Text;
+const array = [1, -4, 2];
+const posArr = array.filter(getPositive);
+
+function getPositive(array: number) {
+    return array > -1;
 }
+
+const joinStr = posArr.join('');
+console.log(joinStr);
+
+
+// // *** H - TASK ***
+// // Shunday function tuzing, u string qabul qilib teskari qilib return qilsin
+// // MASALAN: gerReverse("hello") return qiladi "olleh"
+// function getReverse(text: string) {
+
+//     const Text = text.split('').reverse().join('');
+//     return Text;
+// }
     
-console.log(getReverse("hello"));
+// console.log(getReverse("hello"));
 
 // // *** G - TASK ***
 // // Shunday function tuzingki unga integerlardan iborat array pass 
