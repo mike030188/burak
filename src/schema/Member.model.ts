5609015a4578f4b422cda02b05a2c2f68780fb19
mike030188 <halol.imnida2023@gmail.com>
2024-02-27 18:57:11 +0500

    feat: develop member schema model

---
+++ src/schema/Member.model.ts
@@ -0,0 +1,59 @@
+import mongoose, { Schema } from "mongoose";
+import { MemberStatus, MemberType } from "../libs/enum/member.enum";
+
+
+const memberSchema = new Schema (
+    {
+        memberType: {
+            type: String,
+            enum: MemberType,
+            default: MemberType.USER,
+            // required: false
+        },
+
+        memberStatus: {
+            type: String,
+            enum: MemberStatus,
+            default: MemberStatus.ACTIVE,
+            // required: false
+        },
+
+        memberNick: {
+            type: String,
+            index: { unique: true, sparse: true }, // nickname faqat 1ta bolishi kk
+            required: true, // DBda bo`lishi talab etiladi
+        },
+
+        memberPhone: {
+            type: String,
+            index: { unique: true, sparse: true }, // tel.raqam faqat 1ta bolishi kk
+            required: true, // DBda bo`lishi talab etiladi
+        },
+
+        memberPassword: {
+            type: String,
+            select: false, // password 
+            required: true, // DBda bo`lishi talab etiladi
+        },
+
+        memberAddress: {
+            type: String,
+        },
+
+        memberDesc: {
+            type: String,
+        },
+
+        memberImage: {
+            type: String,
+        },
+
+        memberPoints: {
+            type: Number,
+            default: 0,
+        },
+    },
+    { timestamps: true } // updatedAt, createdAt
+);
+
+export default mongoose.model("Member", memberSchema);
