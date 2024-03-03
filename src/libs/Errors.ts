8a246960155baece893faa1069c8f95171a409df
mike030188 <halol.imnida2023@gmail.com>
2024-03-03 16:01:29 +0500

    feat: develop restaurant login process

--- src/libs/Errors.ts
+++ src/libs/Errors.ts
@@ -9,11 +9,17 @@ export enum HttpCode {
     INTERNAL_SERVER_ERROR = 500,
   }
   
+  /** CUSTOMIZED MESSAGES**/
   export enum Message {
     SOMETHING_WENT_WRONG = "Something went wrong!",
     NO_DATA_FOUND = "No data is found!",
     CREATE_FAILED = "Create is failed!",
     UPDATE_FAILED = "Update is failed!",
+
+    USED_NICK_PHONE = "You are inserting already used nick or phone!",
+    NO_MEMBER_NICK = "No member with that member nick!",
+    WRONG_PASSWORD = "Wrong password intered, please try again!",
+
   }
   
   class Errors extends Error {
--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -1,5 +1,5 @@
 import MemberModel from "../schema/Member.model";
-import { Member, MemberInput } from "../libs/types/member";
+import { LoginInput, Member, MemberInput } from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import { MemberType } from "../libs/enums/member.enum";
 
@@ -26,6 +26,32 @@ class MemberService {
         } catch (err) {
           throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
         }
+      }
+    
+      public async processLogin(input: LoginInput): Promise<Member> {
+        const member = await this.memberModel
+          .findOne(
+            { memberNick: input.memberNick }, 
+            { memberNick: 1, memberPassword: 1 } // _id: 0, by default: 1 ga teng
+          )
+          .exec();
+        if (!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
+
+        const isMatch = input.memberPassword === member.memberPassword;
+        // console.log("isMatch:", isMatch);
+        if (!isMatch) {
+          throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD);
+        }
+
+        // const result = await this.memberModel.findById(member._id).exec();        
+        // console.log("result:", result);
+        // return result;
+
+        /** yuqoridagi kodni bu yerda togridan togri return qiliw**/
+        return await this.memberModel.findById(member._id).exec();        
+
+
+
       }
 }
 
