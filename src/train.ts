1af7122b8627e5db2e373a94a4ef47ef5331615f
mike030188 <halol.imnida2023@gmail.com>
2024-03-05 09:53:37 +0500

    task: MIT task-II is done

--- src/train.ts
+++ src/train.ts
@@ -18,20 +18,82 @@ console.log("Hello World");
   ...
 */
 
+
 // *** I - TASK ***
-// shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib,
-//  faqat positive qiymatlarni olib string holatda return qilsin.
-// MASALAN: getPositive([1, -4, 2]) return qiladi "12"
+// Shunday function tuzing, unga string argument pass bolsin.
+// Function ushbu agrumentdagi digitlarni yangi stringda return qilsin
+// MASALAN: getDigits("m14i1t") return qiladi "141"
+ function getDigits(str: string) {
+      
+  const digitMatches = str.match(/\d/g);  // Regex syntax in JavaScript "/\d" matches a decimal digit character (i.e. 0-9). "g" - globally
 
-const array = [1, -4, 2];
-const posArr = array.filter(getPositive);
-
-function getPositive(array: number) {
-    return array > -1;
+  // Berilgan string ichidan raqamlar bor yoqligini check qilamiz
+  if (digitMatches) {
+    //  
+    return digitMatches.join('');
+  } else {
+    // Agar raqam topolmasang, return 0
+    return 0;
+  }
 }
+    
+  const str = "m14i1t";
+  const onlyNumbers = getDigits(str);
+  console.log(`natija: ${onlyNumbers}`);
 
-const joinStr = posArr.join('');
-console.log(joinStr);
+
+
+/**** 2nd Challenge task *****/
+// // SEZAR SECRET MESSAGING
+// const alphabet = "abcdefghijklmnopqrstuvwxyz";
+// // console.log(alphabet.length);
+// let code: number = 2;
+// // let code: number = 5;
+
+// function shiftLetter(letter: string, shift: number) {
+//   const index: number = alphabet.indexOf(letter.toLowerCase());
+//   if (index === -1) {
+//     // bu yerda agar "character" - alphabetga tegishli bolmasa uni prosto tawab ket...
+//     return letter; 
+//   }
+//   // bu yerda yangi indexni aniqlaymiz
+//   const newIndex: number = (index + shift + alphabet.length) % alphabet.length;
+//   return alphabet[newIndex];
+
+//  }
+
+// function decodeMessage(message: string, secret: any) {
+//  return message.split("").map((char) => shiftLetter(char.toLowerCase(), secret)).join("");
+
+// }
+
+// const message = "hello, how are you doing, my general?";
+// // const message = "yes, hello, how are you doing, my general?";
+// const secret_msg = decodeMessage(message, code);
+// console.log("Secret => ", secret_msg);
+
+// setTimeout( () => {
+//   code *= -1;
+//   const message_encoded = decodeMessage(secret_msg, code);
+//   console.log("Original => ", message_encoded);
+// }, 5000);
+
+
+
+// // *** I-0 - TASK ***
+// // shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib,
+// //  faqat positive qiymatlarni olib string holatda return qilsin.
+// // MASALAN: getPositive([1, -4, 2]) return qiladi "12"
+
+// const array = [1, -4, 2];
+// const posArr = array.filter(getPositive);
+
+// function getPositive(array: number) {
+//     return array > -1;
+// }
+
+// const joinStr = posArr.join('');
+// console.log(joinStr);
 
 
 // // *** H - TASK ***
